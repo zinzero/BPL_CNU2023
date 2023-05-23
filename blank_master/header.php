6a9fc065e5867a8de25753a40644857a4e546f50
zinzero <realbloom0122@gmail.com>
2023-05-24 03:14:53 +0900

    zinzero chgProfile, chgPW, login

---
+++ blank_master/header.php
@@ -0,0 +1,35 @@
+<!-- <!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loginview.css">
+    <title>Document</title>
+</head>
+<body> -->
+<header>
+        <div class="container">
+            <div class="logo">
+                <a href="mainpage.html">
+                    <img src="img/logo.png" alt="logo">
+                </a>
+            </div>
+            <div class="menu">
+                <a href="">필기 페이지</a>
+                <a href="">문제 페이지</a>
+                <a href="">공유 페이지</a>
+            </div>
+            <div class="hamburger"></div>
+        </div>
+        <section id="search" class="container">
+            <p>검색</p>
+            <input type="text" placeholder="검색어를 입력해주세요.">
+            <button type="button">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </section>
+</header>
+<!--     
+</body>
+</html> -->
